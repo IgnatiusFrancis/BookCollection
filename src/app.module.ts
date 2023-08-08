6051ceff144226e210cd6a0932cadff7003f062f
IgnatiusFrancis <ignatiuzzfrank@gmail.com>
2023-08-08 14:01:33 +0100

    Generated book resources and created prisma schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [BooksModule],
   controllers: [AppController],
   providers: [AppService],
+  imports: [BooksModule, PrismaModule],
 })
 export class AppModule {}
